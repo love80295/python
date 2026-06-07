89ab8b0c5a756edc150aa32eb9518cfeeeeb0834
loveagrawal <loveagrawal80295@gmail.com>
2026-06-07 15:45:20 +0530

    overview

---
+++ BASIC/Data_types.py
@@ -0,0 +1,15 @@
+# comment 
+# numbers -- > 123 , 12.3542 , 3+47 , 0b111 , Ddecimal() , Fraction()
+# List --> continuos memeory space [1,2,[4.234] , List(range(10))]
+# tuple --> (1,2,3,4,5,6,7,); 
+# dictionsary = {'thw':'sdfjh' , 'sddf' : 'sdfs'}; dictionary does not start with zero
+#  in key value pair
+# sets --> hold unique value 
+# file
+# boolean --> true , false;
+#none
+# functions
+# classes
+# modules
+# advance --> decorators , iterators , generator , meta programming
+print(2**10000);
