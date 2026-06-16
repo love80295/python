1f7cec9f65166f39acae64d2221f967f42150005
loveagrawal <loveagrawal80295@gmail.com>
2026-06-16 12:58:51 +0530

    oops in python basic

---
+++ OOPs/basic_class_obj.py
@@ -0,0 +1,7 @@
+class Car :
+    def __init__ (self , brand , name):
+        self.brand = brand
+        self.name = name
+my_car = Car("lambo" , "urus");
+print(my_car.brand);
+print(my_car.name);
